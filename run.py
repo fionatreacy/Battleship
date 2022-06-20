8c51d964ea3fa483f9119337bea90ac877366801
fionatreacy <fionatreacy00@gmail.com>
2022-06-20 14:02:40 +0000

    add funtions to place ship in random spot on board

--- run.py
+++ run.py
@@ -1,17 +1,30 @@
+from random import randint
 
-
-"""
-create board 
-"""
 board = []
+"""
+Creates 5x5 game board 
+"""
 for x in range(5):
     board.append(["0"] * 5)
 
 def format_board(board):
     for i in board:
-        print(i)
+        print(" ".join(i))
 
-print("Let's play Battleship!")
+print("Welcome to Battleship!")
 print("Board size: 5x5. Number of ships: 1. Number of missiles: 3. \n")
 print("Top left corner is row: 0, column: 0. Please enter co-ordinates from 0-4\n")
 format_board(board)
+
+""" 
+Places ship in random location on board
+"""
+
+def rand_column(board):
+    return randint(0, len(board) -1)
+
+def rand_row(board):
+    return randint(0, len(board) -1)
+
+ship_column = rand_column(board)
+ship_row = rand_row(board)
