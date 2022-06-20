c67cb4a242decc5301974d2571c7763b4f6c9d1b
fionatreacy <fionatreacy00@gmail.com>
2022-06-20 13:49:59 +0000

    add game instructions for user

--- run.py
+++ run.py
@@ -12,4 +12,6 @@ def format_board(board):
         print(i)
 
 print("Let's play Battleship!")
+print("Board size: 5x5. Number of ships: 1. Number of missiles: 3. \n")
+print("Top left corner is row: 0, column: 0. Please enter co-ordinates from 0-4\n")
 format_board(board)
