ad5b22a7f6b9f6ebb55c75e28983791122586c4f
fionatreacy <fionatreacy00@gmail.com>
2022-06-20 15:58:20 +0000

    added comments to functions

--- run.py
+++ run.py
@@ -1,62 +1,68 @@
 from random import randint
 
-
-"""
-Creates 5x5 game board 
-"""
 board = []
+"""
+Creates 5x5 game board
+"""
 for x in range(5):
     board.append(["0"] * 5)
+
 
 def format_board(board):
     for i in board:
         print(" ".join(i))
 
 
-"""
-Game intro 
-"""
 print("Welcome to Battleship!")
-print("Board size: 5x5. Number of ships: 1. Number of missiles: 3. \n")
-print("Top left corner is row: 0, column: 0. Please enter co-ordinates from 0-4\n")
+print("Board size: 5x5. \n")
+print("Number of ships: 1. Number of missiles: 3. \n")
+print("Top left corner is row: 0, column: 0. \n")
 format_board(board)
-
-
-""" 
-Places ship in random location on board
 """
+Prints game intro + instructions
+"""
+
+
 def rand_column(board):
-    return randint(0, len(board) -1)
+    return randint(0, len(board) - 1)
+
 
 def rand_row(board):
-    return randint(0, len(board) -1)
+    return randint(0, len(board) - 1)
+
 
 ship_column = rand_column(board)
 ship_row = rand_row(board)
-
-
-""" 
-Compare players guess to the ships location and gives a message if the player hits, misses, or has repeated a guess.
 """
+Places ship in random location on board
+"""
+
+
 for attempts in range(3):
-    print("Attempts", attempts + 1)
-    """ 
+    """
     Repeats the game until all attempts have been used or player wins
     """
-    """ 
-    Allows player to guess a location
-    """
-    guess_row = int(input("Guess Row: \n"))
-    guess_column = int(input("Guess Column: \n"))
-    
+    print("Attempts", attempts + 1)
+
+    guess_row = int(input("Guess Row 0-4 : \n"))
+    guess_column = int(input("Guess Column 0-4 : \n"))
+
+
     if ship_row == guess_row and ship_column == guess_column:
+        """
+        The following compares players guess to the ships location and gives
+        a message if the player hits, misses, or has repeated a guess.
+        """
         print("You've sunk the Battleship!")
         board[ship_row][ship_column] = "X"
         print(format_board(board))
 
     else:
-        if (guess_column > 5 or guess_row > 5) or (guess_column <0 or guess_row < 0):
-            print("Those are land co-ordinates, please enter a number from 0-4")
+        if (guess_column > 5 or guess_row > 5):
+            print("Those are land co-ordinates, try again")
+            print(format_board(board))
+        elif (guess_column < 0 or guess_row < 0):
+            print("Those are land co-ordinates, try again")
             print(format_board(board))
         elif board[guess_row][guess_column] == "-":
             print("You've guessed that one already, try again.")
