9474d62f5f6c7ee5811be87b6a839f26e101442d
fionatreacy <fionatreacy00@gmail.com>
2022-06-20 13:42:44 +0000

    add format_board function to format board into 5x5 square

--- run.py
+++ run.py
@@ -6,3 +6,10 @@ create board
 board = []
 for x in range(5):
     board.append(["0"] * 5)
+
+def format_board(board):
+    for i in board:
+        print(i)
+
+print("Let's play Battleship!")
+format_board(board)
